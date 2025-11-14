d2e8bf3e00b27c2552df64204bbfcd83acea3acb
cobl703 <cobll703@gmail.com>
2025-11-14 20:22:59 +0530

    Reformat chance predicates

--- quasar-v2/data/datapipes_lib/predicate/chances/generate.py
+++ quasar-v2/data/datapipes_lib/predicate/chances/generate.py
@@ -1,11 +1,11 @@
-referencer = '{"condition": "random_chance", "chance": $CHANCE}'
-
-# Generate model
-def genChancePredicate(chance):
-    contents = referencer
-    with open(str(chance) + '.json', 'w') as file:
-        chance /= 100
-        file.write(contents.replace('$CHANCE', str(chance)))
-
-for x in range(1, 100+1):
-    genChancePredicate(x)
+import os
+def gen_chance_predicate(chance: float) -> None:
+    with open(f'{os.path.dirname(__file__)}/{chance}.json', 'w') as file:
+        file.write(f'{{\n  "condition": "random_chance",\n  "chance": {chance/100}\n}}')
+gen_chance_predicate(0.01)
+gen_chance_predicate(0.02)
+gen_chance_predicate(0.05)
+gen_chance_predicate(0.1)
+gen_chance_predicate(0.3)
+for chance in range(1, 100+1):
+    gen_chance_predicate(chance)
